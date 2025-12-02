137a1e1d1f9f5942bece5895b94e5653b4952d46
victorkyulu_coder <victorkyulu@gmail.com>
2025-12-02 11:05:46 +0300

    removed some errors for deployment

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import * as IMAGES from "../images";
+import { compressImage } from "../utils/imageCompressor";
+
 const Items = [
   { id: 1, item: "Apartments", price: 1500, image: IMAGES.apartments },
   { id: 2, item: "Bull", price: 1200, image: IMAGES.bull },
